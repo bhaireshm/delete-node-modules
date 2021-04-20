cc9ff3a3b4d97c3bfbca0082bf9a4965d324d9b7
Bhairesh <bhairesh@arithaconsulting.com>
2021-04-20 19:10:05 +0530

    path changes.

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 // get the current folder
-const fs = require('fs');
-const readline = require('readline');
+const fs = require("fs");
+const readline = require("readline");
+const path = require("path");
+const { exit } = require("process");
 
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout,
 });
 
 var pathName;
@@ -13,72 +15,86 @@ var deleteFolderName = "node_modules";
 readLine();
 
 function readLine() {
-    rl.question("Enter Complete path: ", (ans) => {
-        pathName = ans;
-        try {
-            if (pathName != null || pathName != undefined) {
-                console.log('\nprocessing please wait...\n');
-                pathName = pathName.replace("\\node_modules", "");
-                const folders = fs.readdirSync(pathName);
-                checkFolder(folders);
-            } else {
-                console.log("Path not found");
-            }
-        } catch (err) {
-            print(err);
-        } finally {
-            rl.close();
-            readLine();
-        }
-    });
+  rl.question("Enter Complete path: ", (ans) => {
+    pathName = ans;
+    try {
+      if (pathName != null || pathName != undefined) {
+        console.log("\nprocessing please wait...\n");
+        pathName = pathName.replace("\\node_modules", "");
+        const folders = fs.readdirSync(pathName);
+        checkFolder(folders);
+      } else {
+        console.log("Path not found");
+      }
+    } catch (err) {
+      print(err);
+    } finally {
+      rl.close();
+      //   readLine();
+    }
+  });
 }
 
 // check all the folders for node_modules folder if present delete it.
 function checkFolder(fldrs) {
-    fldrs.forEach(fldr => {
-        const stats = fs.statSync(fldr);
-//         console.log(fldr);
+  fldrs.forEach((fldr) => {
+    // const stats = fs.statSync(fldr);
+    // console.log(fldr);
 
-        if (fldr == deleteFolderName) {
-            let name = pathName + '\\' + fldr + '\\';
+    if (fldr == deleteFolderName) {
+      // pathName + '\\' + fldr + '\\'
+      let name = path.join(pathName, fldr, "\\");
+      //   console.log(name);
 
-            let packageFolders = fs.readdirSync(name);
-            packageFolders.forEach(pf => {
-                deleteFolder(`${name}\\${pf}`);
-            });
-        } else {
-            // check for the inner folders
+      let packageFolders = fs.readdirSync(name);
+      if (packageFolders.length > 0) {
+        packageFolders.forEach((pf) => {
+          const p = path.join(name, pf);
+          // console.log(p);
 
-            if (stats.isDirectory()) console.log("node_modules not found in " + fldr + " folder");
-            return;
-        }
-    });
+          deleteFolder(p); //${name}\\${pf}
+        });
+        
+        exit(0);
+        
+      } else {
+        console.log(new Error("node_modules folder is empty"));
+        deleteFolder(fldr);
+      }
+    } else {
+      //   console.log(new Error("node_modules folder is empty / not found"));
+      // check for the inner folders
+      //   if (stats.isDirectory())
+      //     console.log("node_modules not found in " + fldr + " folder");
+      //   return;
+    }
+  });
 }
 
 function checkChildFolder(foldrName) {
-    let packageFolders = fs.readdirSync(foldrName);
-    packageFolders.forEach(pf => {
-        checkChildFolder(pf);
-        deleteFolder(`${foldrName}\\${pf}`);
-    });
+  let packageFolders = fs.readdirSync(foldrName);
+  packageFolders.forEach((pf) => {
+    checkChildFolder(pf);
+    deleteFolder(`${foldrName}\\${pf}`);
+  });
 }
 
 function deleteFolder(path) {
-    try {
-        fs.rmdirSync(path, {
-            recursive: true,
-            retryDelay: 100
-        }); // removes folder and its content
-        console.log('Deleted Path : ' + path);
-    } catch (err) {
-        print(err);
-    }
+  try {
+    fs.rmdirSync(path, {
+      recursive: true,
+      retryDelay: 100,
+    }); // removes folder and its content
+    console.log("Deleted Path : " + path);
+  } catch (err) {
+    print(err);
+  }
 }
 
 function print(err) {
-    console.log("----------------------------------------------");
-//     console.log(err.info);
-    console.error(err.message);
-    console.log("----------------------------------------------");
-    return;
+  console.log("----------------------------------------------");
+  console.log(err);
+  console.error(err.message);
+  console.log("----------------------------------------------");
+  return;
 }
